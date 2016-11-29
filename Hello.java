2186606c92cfa345102fa7872cf9bbab7debc4a6
Shin HM <shm6708@naver.com>
2016-11-29 11:16:52 +0900

    Hello his git

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hellow Git~");
 		System.out.println("Hello my java");
+		System.out.println("HELLO HIS GIT");
 
 	}	
 }
